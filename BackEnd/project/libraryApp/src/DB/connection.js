9118d42cd6a41c079d1a62e5a344dacd4791c5b0
meellsayed <meellsayed@gmail.com>
2026-09-02 09:10:00 +0300

    before docker

--- BackEnd/project/libraryApp/src/DB/connection.js
+++ BackEnd/project/libraryApp/src/DB/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   const dbUrl =
-    process.env.DB_URL || `mongodb://localhost:27017/${process.env.APP_NAME}`;
+    process.env.DB_URI || `mongodb://localhost:27017/${process.env.APP_NAME}`;
   await mongoose
     .connect(dbUrl, {
       serverSelectionTimeoutMS: 5000,
